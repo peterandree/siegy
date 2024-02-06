a9bfb2d2ab3784007e1acdb0d75c8b3577269676
peterandree <peterandree@yahoo.de>
2024-02-06 10:48:23 +0100

    prep for soli stop

--- SiegyFinances/FinancialData/FinancialConstants.cs
+++ SiegyFinances/FinancialData/FinancialConstants.cs
@@ -2,9 +2,11 @@
 {
     internal static class FinancialConstants
     {
+        internal const int THE_YEAR_THEY_STOPPED_THE_SOLI = 9999;
+        
         internal const decimal DEFAULT_ONE_TIME_RATE = 720m;
 
-        internal static decimal TAX_ADJUSTMENT(int p_year) => p_year <= 9999 ? TAX_ADJUSTMENT_WITH_SOLI : TAX_ADJUSTMENT_WITHOUT_SOLI;
+        internal static decimal TAX_ADJUSTMENT(int p_year) => p_year <= THE_YEAR_THEY_STOPPED_THE_SOLI ? TAX_ADJUSTMENT_WITH_SOLI : TAX_ADJUSTMENT_WITHOUT_SOLI;
 
         internal const decimal TAX_ADJUSTMENT_WITH_SOLI = 0.26375m;
         internal const decimal TAX_ADJUSTMENT_WITHOUT_SOLI = 0.25m;
